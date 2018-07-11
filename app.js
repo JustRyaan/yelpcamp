a2b4ededd0523c3501e9febacb5ec03f32e51c51
Ryan <ryanpetermitchell@gmail.com>
2018-07-11 22:33:13 +0000

    added dbURL environment variable

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
 
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://mitch:h07713cA1nE@ds135061.mlab.com:35061/yelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://mitch:h07713cA1nE@ds135061.mlab.com:35061/yelpcamp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
